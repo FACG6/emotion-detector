2414307771f1b42abdbf42eff761d6482e87d49e
aminalakhsham <aminkng140@gmail.com>
2019-02-05 14:18:25 +0200

    add home and page not found Relates #7

--- src/handlers.js
+++ src/handlers.js
@@ -23,22 +23,22 @@ const handleHome = () => {
 
 };
 
-const handleStatic = () => {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const handleStatic = (req, res, endpoint) => {
+    const filePath = path.join(__dirname,"..",...(endpoint.split("/")));
+    const fileTypes = {
+        css: "text/css",
+        js: "text/javascript",
+      };
+    const ext = path.extname(endpoint).split('.')[1]
+      fs.readFile(filePath, (err,file)=>{
+          if(err){
+            handleServerError(req, res);
+          }else{
+            res.writeHead(200,{"content-type":`${fileTypes[ext]}`})
+            res.write(file)
+            res.end()
+          }
+      })
 
 
 
@@ -55,6 +55,9 @@ const handleImg = () => {};
 const handleNotFoundError = () => {
 
 
+    response.writeHead(404,{"Content-Type": "text/html"});
+      response.write("<h1>404 Page not found !!!</h1>");
+      response.end();
 
 
 
@@ -75,14 +78,13 @@ const handleNotFoundError = () => {
 
 
 
-
-
-
-
+      
 
 };
 
-const handleServerError = () => {
+const handleServerError = (req, res) => {
+
+   
 
 
 
@@ -108,8 +110,6 @@ const handleServerError = () => {
 
 
 
-
-    
 };
 
 module.exports = {

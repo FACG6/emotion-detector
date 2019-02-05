bc751674af2f7d9bfbd05cfef695b6ee8c029f99
aminalakhsham <aminkng140@gmail.com>
2019-02-05 13:46:38 +0200

    and our handlers function Relates #5

---
+++ src/handlers.js
@@ -0,0 +1,121 @@
+const path = require("path");
+const fs = require("fs");
+
+const handleHome = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+const handleStatic = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+const handleImg = () => {};
+
+const handleNotFoundError = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
+
+const handleServerError = () => {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+};
+
+module.exports = {
+  handleHome,
+  handleStatic,
+  handleImg,
+  handleNotFoundError,
+  handleServerError
+};

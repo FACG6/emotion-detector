0d13347d08e083c8dc085d0b12d55d94b93710d6
aminalakhsham <aminkng140@gmail.com>
2019-02-06 14:46:10 +0200

    some styles and doms

--- public/js/fetch.js
+++ public/js/fetch.js
@@ -1,15 +1,13 @@
 const fetch = (method, url, value, callback) => {
-    const xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState === 4)
-            if (xhr.status === 200) {
-                if (JSON.parse(xhr.responseText))
-                    callback(null, JSON.parse(xhr.response));
-                else
-                    callback(new Error('Error'));
-            } else
-                callback(new Error('Api is not responding'));
-    }
-    xhr.open(method, url);
-    xhr.send(value);
-}
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState === 4)
+      if (xhr.status === 200) {
+        if (JSON.parse(xhr.responseText))
+          callback(null, JSON.parse(xhr.response), value);
+        else callback(new Error("Error"));
+      } else callback(new Error("Api is not responding"));
+  };
+  xhr.open(method, url);
+  xhr.send(value);
+};
